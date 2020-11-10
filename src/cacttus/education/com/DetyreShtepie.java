91bbb1dd71b38f35ea255e5c3cb8b333247d0a77
Naim Sulejmani <naim.sulejmani@gmail.com>
2020-11-10 20:54:48 +0100

    detyre shtepie

---
+++ src/cacttus/education/com/DetyreShtepie.java
@@ -0,0 +1,44 @@
+package cacttus.education.com;
+
+import java.util.Scanner;
+
+public class DetyreShtepie {
+    public static void main(String[] args) {
+        System.out.println("Ligji njutonit");
+        ligjiDyteINjutonit();
+
+        System.out.println("Ligji gravitetit");
+        ligjiGravitetit();
+    }
+
+    public static void ligjiDyteINjutonit() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Shkruaj masen e trupit: ");
+        double masa = reader.nextDouble();
+
+        System.out.println("Shkruaj nxitimin e trupit: ");
+        double nxitimi = reader.nextDouble();
+
+        double forca = masa * nxitimi; // F = m * a;
+
+        System.out.printf("Forca eshte e barabarte: %.2f N",forca);
+
+    }
+
+    public static void ligjiGravitetit() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Shkruaj masen e trupit: ");
+        double masa = reader.nextDouble();
+
+        System.out.println("Shkruaj gravitetin: ");
+        double graviteti = reader.nextDouble();
+
+        double forca = masa * graviteti; // F = m * a;
+
+        System.out.printf("Forca eshte e barabarte: %.2f N",forca);
+    }
+
+    public static void syprinaKatrorit() {
+
+    }
+}
---
+++ src/cacttus/education/com/Koha.java
@@ -0,0 +1,11 @@
+package cacttus.education.com;
+
+import java.util.GregorianCalendar;
+
+public class Koha {
+    public static void main(String[] args) {
+        GregorianCalendar calendar = new GregorianCalendar();
+        System.out.println(calendar.getTime());
+
+    }
+}
