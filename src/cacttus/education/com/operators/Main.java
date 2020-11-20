6dd436692e476025906805c69527ff02a32be4cf
Naim Sulejmani <naim.sulejmani@gmail.com>
2020-11-20 18:34:03 +0100

    operators mix

--- src/cacttus/education/com/operators/Main.java
+++ src/cacttus/education/com/operators/Main.java
@@ -1,5 +1,7 @@
 package cacttus.education.com.operators;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int b = 5;
@@ -12,5 +14,40 @@ public class Main {
 
         System.out.println(quadraticNegative);
         System.out.println(quadraticPositive);
+
+        int a = 17;
+        b = 29;
+
+        System.out.println(a & b);
+        System.out.println(~(a ^ b));
+
+        a = 8;
+
+        System.out.println(a >> 1);
+        System.out.println(a << 2);
+        System.out.println(17 >>> 32);
+
+
+        int year = 2019;
+        boolean isLeapYear;
+        isLeapYear = (year % 4 == 0) && (year % 100 != 0);
+
+        isLeapYear = isLeapYear || (year % 400 == 0);
+        //variabla boolean mund te llogaritet si shprehje krahasuese
+        // 1==3
+        System.out.println(isLeapYear == true ? "Eshte vit i brishte!" : "Eshte vit normal!");
+        System.out.println(isLeapYear ? "Eshte vit i brishte!" : "Eshte vit normal!");
+
+
+//        int numri = 123972 % 3 == 0 ? 10 : 20;
+//        System.out.println("Numri = " + numri);
+
+        double tatimPerDite = 1.75;
+        double totalTatimi = isLeapYear ? tatimPerDite * 366 : tatimPerDite * 365;
+
+        double totalTatimi1 = tatimPerDite * (isLeapYear ? 366 : 365);
+
+        System.out.println("Tatimi = " + totalTatimi);
+
     }
 }
