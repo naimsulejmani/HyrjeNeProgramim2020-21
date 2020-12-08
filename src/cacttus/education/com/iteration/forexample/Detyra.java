33a06ae4f2298782551e6b5a0c31bb192a1b656e
Naim Sulejmani <naim.sulejmani@gmail.com>
2020-12-08 20:52:42 +0100

    for-detyra2 dhe ide per 3

--- src/cacttus/education/com/iteration/forexample/Detyra.java
+++ src/cacttus/education/com/iteration/forexample/Detyra.java
@@ -2,13 +2,23 @@ package cacttus.education.com.iteration.forexample;
 
 public class Detyra {
     public static void main(String[] args) {
-        detyra1();
+        detyra2();
     }
+
     //Prej 1 deri ne 100 shtypi numrat me rend dhe katrorin e tyre
     public static void detyra1() {
-        System.out.printf("n %20s %n","n^2");
+        System.out.printf("n %20s %n", "n^2");
         for (int i = 1; i <= 10; i++) { // i - index, count, counter
             System.out.printf("%d %20d %n", i, i * i);
+        }
+    }
+
+
+    //Prej 1 deri ne 100 shtypi numrat me rend dhe katrorin e tyre, kubin dhe rrenjen katror
+    public static void detyra2() {
+        System.out.printf("n %20s %20s %20s %n", "n^2", "n^3", "sqrt(n)");
+        for (int i = 1; i <= 10; i++) { // i - index, count, counter
+            System.out.printf("%d %20d %20.2f %20.2f  %n", i, i * i, Math.pow(i, 3), Math.sqrt(i));
         }
     }
 }
