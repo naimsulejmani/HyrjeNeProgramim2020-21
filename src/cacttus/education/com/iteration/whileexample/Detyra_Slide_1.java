3a6cf79d23c2b0db3ee97499fbd5441d9dd113cd
Naim Sulejmani <naim.sulejmani@gmail.com>
2020-12-17 21:03:07 +0100

    While detyra vazhdim

--- src/cacttus/education/com/iteration/whileexample/Detyra_Slide_1.java
+++ src/cacttus/education/com/iteration/whileexample/Detyra_Slide_1.java
@@ -7,7 +7,8 @@ public class Detyra_Slide_1 {
         //printNrWithSquare_MXH();
         //sumTilHundred_RS();
         //sumVA();
-        palindromeVA();
+        //palindromeVA();
+        shtypNumratPrej1DeriN_Nga3();
     }
 
     public static void printNrWithSquare_MXH() {
@@ -50,15 +51,11 @@ public class Detyra_Slide_1 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Shkruaj Tekstin");
         String teksti = scan.nextLine();
-
         int i = 0; //charIndex -> prej cilit indeks kemi me fillu me shiku
         //per te kontrolluar palindrome shkojme shkronjen e pare me te fundit
         // te dyten me te parafundit (shkojme deri tek shkronja e mesit)
-
         //nje variabel kontrolluese supozuese
         boolean isPalindrome = true;
-
-
         //Perderisa nuk kemi mri ne mes te karaktereve
         //dhe teksti eshte akoma palindrome shiko karakterin tjeter
         while (i <= teksti.length() / 2 && isPalindrome) {
@@ -69,10 +66,59 @@ public class Detyra_Slide_1 {
             }
             i++; // shko tek indeksi i shkronjes tjeter
         }
-
         System.out.printf("Teksti i dhene %s palindrom!",
                 isPalindrome ? "eshte" : "nuk eshte"
         );
+    }
+
+    public static void numratCift() {
+        int nr = 2;
+        while (nr <= 100) {
+            System.out.println(nr);
+            nr += 2; //rrite numrin per 2
+        }
+    }
+
+    public static void numratTek() {
+        int nr = 1;
+        while (nr <= 100) {
+            System.out.println(nr);
+            nr += 2; //rrite numrin per 2
+        }
+    }
+
+    public static void shkronjatEVogla_GJB() {
+        char shkronja = 'a'; //'a' ose 97
+        while (shkronja <= 'z') { //ose 122
+            System.out.println(shkronja);
+            shkronja++;
+        }
+    }
+
+
+    public static void shtypNumratPrej1DeriN_Nga3() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Shtyp numrin:");
+        int numri = reader.nextInt();
+        int count = 1;
+        while (count <= numri) {
+            System.out.printf("%d %s", count,
+                    count % 3 != 0 ? "," : "\n");
+
+            //if (count % 3 == 0) System.out.println();
+
+            count++;
+        } //masi te perfundon while
+
+        //gjej numrin e mbejtes
+        count = numri % 3;
+        //dhe shko deri ne 3
+        while(count>0 && count<3 ) {
+            System.out.printf("%d %s", 0,
+                    count % 3 != 2 ? "," : "");
+            count++;
+        }
+
     }
 }
 
