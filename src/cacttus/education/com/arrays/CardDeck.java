b67d89bf260df9bf140cb5c77e87b786f684d613
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-01-08 00:02:07 +0100

    deckcard

--- src/cacttus/education/com/arrays/CardDeck.java
+++ src/cacttus/education/com/arrays/CardDeck.java
@@ -3,6 +3,11 @@ package cacttus.education.com.arrays;
 public class CardDeck {
 
     public static void main(String[] args) {
+        //slideExample();
+        viktorCardDeck();
+    }
+
+    public static void slideExample() {
         String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
         String[] suit = {"♣", "♦", "♥", "♠"};
         String[] deck = new String[52];//0..51
@@ -14,7 +19,26 @@ public class CardDeck {
                 //System.out.printf("%3s", rank[rankIndex]);
                 deck[suitIndex * rank.length + rankIndex] = rank[rankIndex] + suit[suitIndex];
             }
-           // System.out.println();
+            // System.out.println();
+        }
+
+        System.out.println("----------------------------------------------");
+        for (int i = 0; i < deck.length; i++) {
+            System.out.printf("%4s", deck[i]);
+        }
+        System.out.println("\n----------------------------------------------");
+    }
+
+    public static void viktorCardDeck() {
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] suit = {"♣", "♦", "♥", "♠"};
+        String[] deck = new String[52];
+
+        int counter = 0;
+        for (int i = 0; i < rank.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                deck[counter++] = rank[i] + suit[j];
+            }
         }
 
         System.out.println("----------------------------------------------");
