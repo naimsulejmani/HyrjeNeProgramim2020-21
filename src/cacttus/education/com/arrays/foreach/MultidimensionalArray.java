c55e2a2d9dc46bc9b5748459516a098fcc9d2f6b
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-01-14 20:59:16 +0100

    zbirtja e matricave

--- src/cacttus/education/com/arrays/foreach/MultidimensionalArray.java
+++ src/cacttus/education/com/arrays/foreach/MultidimensionalArray.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class MultidimensionalArray {
     public static void main(String[] args) {
         //rsSlideMatrix();
-        slide1detyra();
+        //slide1detyra();
+        zbritjaMatrices();
     }
 
     public static void rsSlideMatrix() {
@@ -35,6 +36,33 @@ public class MultidimensionalArray {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.printf("%10d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    //&nbsp; <p>&nbsp;&nbsp;&nbsp;&nbsp;NAIM
+    private static void zbritjaMatrices() {
+        Scanner reader = new Scanner(System.in);
+        int[][] a = new int[2][2];
+        for (int rowIndex = 0; rowIndex < a.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < a[rowIndex].length; columnIndex++) {
+                System.out.printf("Shkruaj vleren per Matricen e pare[%d, %d]: ", rowIndex + 1, columnIndex + 1);
+                a[rowIndex][columnIndex] = reader.nextInt();
+            }
+        }
+        int[][] b = new int[2][2];
+        for (int rowIndex = 0; rowIndex < b.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < b[rowIndex].length; columnIndex++) {
+                System.out.printf("Shkruaj vleren per Matricen e dyte[%d, %d]: ", rowIndex + 1, columnIndex + 1);
+                b[rowIndex][columnIndex] = reader.nextInt();
+            }
+        }
+        int c[][] = new int[2][2];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                c[i][j] = a[i][j] - b[i][j];
+                System.out.print(c[i][j] + " ");
             }
             System.out.println();
         }
